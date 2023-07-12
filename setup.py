ed9c261daa8b12ee769fac50322b15fe750f5c8b
alec-roberson <alectroberson@gmail.com>
2023-07-12 11:44:37 -0700

    reorganization and added setup.py and MANIFEST.in so the package is pip installable

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='lab_framework',
+    packages=['lab_framework'],
+    version='1.0.0',
+    description='Framework for controlling many components of the lab.',
+    url='https://github.com/Lynn-Quantum-Optics/lab_framework',
+    author='Alec Roberson',
+    author_email='alectroberson@gmail.com',
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'uncertainties', 
+        'scipy',
+        'serial',
+        'tqdm',
+        'thorlabs_apt'
+    ]
+)
