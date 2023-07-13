d57b3d6d3fe6b95c7441829bc8d27d11d8eeacc5
Alec Roberson <alectroberson@gmail.com>
2023-07-13 13:13:47 -0700

    updating package init to import general base classes as well

--- lab_framework/__init__.py
+++ lab_framework/__init__.py
@@ -1,12 +1,12 @@
-''' __init__.py
+''' Lab Framework Package
 
-This file allows for the core folder to behave as a module from which we may import objects that are brought into this file.
+Author: Alec Roberson (aroberson@hmc.edu / alectroberson@gmail.com) 2023
 
-Author(s):
-- Alec Roberson (aroberson@hmc.edu) 2023
+This package contains the classes, functions, and modules necessary to manage the experimental setup in the laboratory. It is currently somewhat specific to our own laboratory setup, however the generalities of the code should make it very easy to extend upon for other setups.
+
 '''
 
 # import relevant objects
-from .monitors import CCU, Laser
-from .motor_drivers import ElliptecMotor, ThorLabsMotor, MOTOR_DRIVERS
+from .monitors import SerialMonitor, CCU, Laser
+from .motor_drivers import Motor, ElliptecMotor, ThorLabsMotor, MOTOR_DRIVERS
 from .manager import Manager
