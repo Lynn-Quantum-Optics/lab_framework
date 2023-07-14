fb9190e974abf80fdf4855781e95bfc16e243ea6
alec-roberson <alectroberson@gmail.com>
2023-07-14 13:21:34 -0700

    updated version number (1.0.6)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='lab_framework',
     packages=['lab_framework'],
-    version='1.0.5',
+    version='1.0.6',
     description='Framework for controlling many components of the lab.',
     url='https://github.com/Lynn-Quantum-Optics/lab_framework',
     author='Alec Roberson',
